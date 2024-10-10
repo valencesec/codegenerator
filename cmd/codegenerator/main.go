a2aecf7a3e8a3d07a2b1b753a5087affaf429821
Shlomi Matichin <shlomomatichin@gmail.com>
2024-10-10 14:19:43 +0300

    flag -dir can be repeated

--- cmd/codegenerator/main.go
+++ cmd/codegenerator/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/valencesec/codegenerator"
 )
 
+type repeatableFlag []string
+// String is an implementation of the flag.Value interface
+func (i *repeatableFlag) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+// Set is an implementation of the flag.Value interface
+func (i *repeatableFlag) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 func main() {
 	inFile := flag.String("in", "", "path to input file")
 	outFile := flag.String("out", "", "path to output file")
-	dirToScan := flag.String("dir", "", "directory to scan")
+	var dirsToScan repeatableFlag
+	flag.Var(&dirsToScan, "dir", "directory to scan")
 	chDir := flag.String("chdir", "", "change directory before scanning")
 	ext := flag.String("ext", ".code_template2", "template extension when scanning dir")
 	dext := flag.String("dext", ".code_template.yaml", "data inline template in file extension when scanning dir")
@@ -23,17 +36,19 @@ func main() {
 		}
 	}
 
-	if *dirToScan != "" {
+	if len(dirsToScan) > 0 {
 		if *inFile != "" || *outFile != "" {
 			panic("-dir is mutual exclusive with -in and -out")
 		}
-		err := codegenerator.ScanFolder(*dirToScan, *ext)
-		if err != nil {
-			panic(err)
-		}
-		err = codegenerator.ScanFolder(*dirToScan, *dext)
-		if err != nil {
-			panic(err)
+		for _, dirToScan := range dirsToScan {
+			err := codegenerator.ScanFolder(dirToScan, *ext)
+			if err != nil {
+				panic(err)
+			}
+			err = codegenerator.ScanFolder(dirToScan, *dext)
+			if err != nil {
+				panic(err)
+			}
 		}
 	} else {
 		if *inFile == "" || *outFile == "" {
